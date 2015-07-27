86886bc4950d697d4b70f40773029749a5cfda5b
Jean-Louis Mbaka <jeanlouis.mbaka@gmail.com>
2015-07-27 08:34:07 +0100

    Parser is feature complete

--- translator.go
+++ translator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -29,9 +30,10 @@ const (
 // convenient access to their compoenents.
 // In addition, it removes all white space and comments.
 type Parser struct {
-	InputFile      string
-	CurrentCommand string
-	Scanner        *bufio.Scanner
+	InputFile          string
+	CurrentCommand     string
+	CurrentCommandType CommandType
+	Scanner            *bufio.Scanner
 }
 
 // Opens the input fiel and gets ready to parse it.
@@ -58,13 +60,19 @@ func (p *Parser) HasMoreCommands() bool {
 // Initially, there is no current cmdS
 func (p *Parser) Advance() {
 	for {
-		token := strings.TrimSpace(p.Scanner.Text())
-		if len(token) > 0 {
-			p.CurrentCommand = token
-			return
+		if isComment(p.Scanner.Text()) == false {
+			token := strings.TrimSpace(p.Scanner.Text())
+			if len(token) > 0 {
+				p.CurrentCommand = token
+				return
+			}
 		}
 		p.Scanner.Scan() // Advance to next token
 	}
+}
+
+func isComment(str string) bool {
+	return strings.Index(str, "//") == 0
 }
 
 // Returns the type of the current cmd.
@@ -72,31 +80,31 @@ func (p *Parser) CommandType() CommandType {
 	tokens := strings.Fields(p.CurrentCommand)
 	switch tokens[0] {
 	case "add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not":
-		fmt.Println("COMMAND TYPE C_ARITHMETIC: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_ARITHMETIC")
 		return CArithmetic
 	case "push":
-		fmt.Println("COMMAND TYPE C_PUSH: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_PUSH")
 		return CPush
 	case "pop":
-		fmt.Println("COMMAND TYPE C_POP: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_POP")
 		return CPop
 	case "label":
-		fmt.Println("COMMAND TYPE C_LABEL: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_LABEL")
 		return CLabel
 	case "goto":
-		fmt.Println("COMMAND TYPE C_GOTO: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_GOTO")
 		return CGoto
 	case "if":
-		fmt.Println("COMMAND TYPE C_IF: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_IF")
 		return CIf
 	case "function":
-		fmt.Println("COMMAND TYPE C_Function: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_Function")
 		return CFunction
 	case "call":
-		fmt.Println("COMMAND TYPE C_POP: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_POP")
 		return CCall
 	case "return":
-		fmt.Println("COMMAND TYPE C_POP: ", p.CurrentCommand)
+		fmt.Println("COMMAND TYPE : C_POP")
 		return CReturn
 	}
 	return -1
@@ -104,12 +112,20 @@ func (p *Parser) CommandType() CommandType {
 
 // Returns the first argument of the current cmd.
 func (p *Parser) Arg1() string {
-	return ""
+	tokens := strings.Fields(p.CurrentCommand)
+	switch p.CurrentCommandType {
+	case CArithmetic:
+		return tokens[0]
+	default:
+		return tokens[1]
+	}
 }
 
 // Returns the second argument of the current cmd.
 func (p *Parser) Arg2() int {
-	return 0
+	tokens := strings.Fields(p.CurrentCommand)
+	d, _ := strconv.ParseInt(tokens[2], 0, 0)
+	return int(d)
 }
 
 func ReadFile() {
@@ -135,9 +151,18 @@ func main() {
 	parser := NewParser(filename)
 	for parser.HasMoreCommands() {
 		parser.Advance()
-		parser.CommandType()
-		parser.Arg1()
-		parser.Arg2()
+		parser.CurrentCommandType = parser.CommandType()
+
+		if parser.CurrentCommandType != CReturn {
+			args1 := parser.Arg1()
+			fmt.Println("ARGS1 : ", args1)
+		}
+		switch parser.CurrentCommandType {
+		case CPush, CPop, CFunction, CCall:
+			args2 := parser.Arg2()
+			fmt.Println("ARGS2 : ", args2)
+		}
+
 		fmt.Println(parser.CurrentCommand)
 		fmt.Println("")
 	}
